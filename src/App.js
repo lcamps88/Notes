1398c2ff4d3dd461699430b8151cea892aa154a8
lcamps88 <lisandra.camps@gmail.com>
2020-07-18 22:07:25 -0400

    Notes2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Notes from './ReduxNotes/main'
-import {Container} from 'semantic-ui-react'
+import {Container, Divider} from 'semantic-ui-react'
 
 function App() {
   return (
     <Container >
+      <Divider hidden/>
       <Notes/>
     </Container>
   );
